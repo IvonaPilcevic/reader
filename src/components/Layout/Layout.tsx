852bbd5a53ada7af5538f5907261b3700a43010a
Ivona Pilcevic <ivona@thinkbig.dev>
2021-08-01 22:15:20 +0200

    lauout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import { Menu } from "../Menu/Menu";
 
 export const Layout = (props: any) => {
-  return <Container>{props.children}</Container>;
+  return (
+    <Container>
+      <Menu />
+      {props.children}
+    </Container>
+  );
 };
