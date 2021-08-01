88837da4b9a5dc5a468dec6e5dd0fa2895d34c68
Ivona Pilcevic <ivona@thinkbig.dev>
2021-08-01 22:33:58 +0200

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,53 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
+import { toJS } from "mobx";
 
-import { Container } from "@material-ui/core";
+import { Container, Grid, List, ListItem } from "@material-ui/core";
+import StarBorder from "@material-ui/icons/StarBorder";
 
-export default function Home() {
+import { useStore } from "../src/store/store";
+import { CustomList } from "../src/components/CustomList/CustomList";
+import { Search } from "../src/components/Search/Search";
+import { Button } from "../src/components/Button";
+
+const Home = observer(() => {
+  const store: any = toJS(useStore());
+
   return (
     <Container>
-      <h1>Home</h1>
+      <Grid container justifyContent="space-around">
+        <Search />
+        <CustomList
+          listName="Reading List"
+          items={store.books}
+          hasButton
+          buttonHref="/reading-list"
+          buttonText="ADD MORE + "
+        />
+
+        <div>
+          <h1>My lists</h1>
+          <List>
+            {Object.keys(store?.readingLists)?.map((listName: any) => (
+              <ListItem key={listName}>
+                <StarBorder color="disabled" /> &nbsp;
+                <span>
+                  {listName}
+                  {/* {item?.title}, {item?.author} ({item?.year}) */}
+                </span>
+              </ListItem>
+            ))}
+          </List>
+
+          <Button
+            href={"/my-lists"}
+            text={"ADD MORE + "}
+            variant="button-dark"
+          />
+        </div>
+      </Grid>
     </Container>
   );
-}
+});
+
+export default Home;
