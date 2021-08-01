106c2add62972e3f55bf0830cc1a48113ab18de9
Ivona Pilcevic <ivona@thinkbig.dev>
2021-08-01 23:04:29 +0200

    display error message

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -6,6 +6,8 @@ export const Search = () => {
   const [searchResult, setSearchResult] = React.useState<any[]>([]);
   const [loading, setLoading] = React.useState<boolean>(false);
   const [query, setQuery] = React.useState<string>("");
+  const [hasError, setHasError] = React.useState<boolean>(false);
+  let errorMessage = "";
 
   React.useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -17,10 +19,13 @@ export const Search = () => {
           .then((res) => res.json())
           .then((data) => {
             setSearchResult(data?.docs);
+            errorMessage = "";
             setLoading(false);
           })
           .catch((error) => {
-            console.error("!error", error);
+            console.error("error", error);
+            setHasError(true);
+            errorMessage = error.toString();
             setLoading(false);
           });
       }
@@ -29,15 +34,23 @@ export const Search = () => {
   }, [query]);
 
   return (
-    <Container>
-      <Input
-        disableUnderline
-        className={(searchResult.length && "search-bar") || "search-bar-empty"}
-        type="text"
-        placeholder="Search by title, author, description, ..."
-        onChange={(e: any) => setQuery(e?.target?.value?.split(" ")?.join("+"))}
-      />
-      <Suggestions suggestions={searchResult} loading={loading} />
-    </Container>
+    <>
+      <Container>
+        <Input
+          disableUnderline
+          className={
+            (searchResult.length && "search-bar") || "search-bar-empty"
+          }
+          type="text"
+          placeholder="Search by title, author, description, ..."
+          onChange={(e: any) =>
+            setQuery(e?.target?.value?.split(" ")?.join("+"))
+          }
+        />
+        <Suggestions suggestions={searchResult} loading={loading} />
+      </Container>
+
+      {hasError && <div style={{ color: "red" }}>{errorMessage}</div>}
+    </>
   );
 };
